267c363ca6e0c98164acecc929aad265e7c8e7d7
Abid Rey Magsambol <abid@Mais-MacBook-Pro.local>
2025-11-10 13:53:26 -0800

    moving all files

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include "../ArduinoXInput/src/XInput.h"
+void setup() {
+// write your initialization code here
+    XInput.begin();
+}
+
+void loop() {
+// write your code here
+}
